beb57394c0df3efa5fb6b46c20738ed969374261
Jesse Harris <jpharris@usc.edu.au>
2018-11-30 14:36:34 +1000

    Added new PackageChecker type

--- Orchestration/PackageChecker.ps1
+++ Orchestration/PackageChecker.ps1
@@ -4,10 +4,14 @@ Param()
 Start-Transcript -Path \\usc.internal\usc\appdev\General\Logs\AutoSequencer.log -Append
 $Working = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
-$DefinedApps='PowerBI.ps1','Firefox-Quantum.ps1','VSCode.ps1','Git.ps1'
+$DefinedApps='PowerBI.ps1','Firefox-Quantum.ps1','VSCode.ps1','Git'
 ForEach ($DefinedApp in $DefinedApps) {
     Write-Verbose "Running test for $DefinedApp at $(Get-Date)" -Verbose
-    & "$Working\$DefinedApp" -Verbose
+    If ((Get-Item "$Working\$DefinedApp").PSIsContainer) {
+        & "$Working\$DefinedApp\CheckPackage.ps1" -Verbose
+    } else {
+        & "$Working\$DefinedApp" -Verbose
+    }
 }
 
 Stop-Transcript
