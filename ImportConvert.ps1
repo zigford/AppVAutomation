947367e064370fd5ddbdebe1bfcf3892658b45b3
Jesse Harris <zigford@gmail.com>
2019-07-25 14:48:44 +1000

    reformat to 80 cols

--- ImportConvert.ps1
+++ ImportConvert.ps1
@@ -93,43 +93,51 @@ Get-ChildItem -Path $PackageRoot | Where-Object {
     }
     #Create Package
     Write-Information "Checking application $Name"
-    Get-ChildItem -Path $SourceFolder.FullName -Recurse | Where-Object {$_.BaseName -match ","} | ForEach-Object {Rename-Item -Path $_.FullName -NewName $_.Name.Replace(",","-")}
-    Switch ($PkgType)
-		{
-			APPV {            
-                New-AppV5Package -Source $SourceFolder -Publisher $Publisher -Name $Name -Version $Version -Description $Description -PackageDest $PackageDest\APPV5Packages
+    Get-ChildItem -Path $SourceFolder.FullName -Recurse |
+    Where-Object {$_.BaseName -match ","} |
+    ForEach-Object {
+        # Strip , and replace with -
+        Rename-Item -Path $_.FullName -NewName $_.Name.Replace(",","-")
+    }
+    Switch ($PkgType) {
+        APPV {
+            New-AppV5Package -Source $SourceFolder -Publisher $Publisher `
+                -Name $Name -Version $Version -Description $Description `
+                -PackageDest $PackageDest\APPV5Packages
+            If ($? -ne $True) {
+                Write-Error "Failed to create application"
+                return
+            }
+        }
+        MSI {
+            $AppPackageXML = Get-AppXML $SourceFolder
+            If ($AppPackageXML) {
+                #Found app descriptor, creating MSI Application
+                New-MSIPackage -Source $SourceFolder `
+                    -Publisher $Publisher -Name $Name `
+                    -Version $Version -Description $Description `
+                    -PackageDest $PackageDest\MSI `
+                    -Descriptor $AppPackageXML
                 If ($? -ne $True) {
                     Write-Error "Failed to create application"
-                    return
-                }
-            }
-            MSI {
-                [xml]$AppPackageXML = (Get-ChildItem -Path $SourceFolder.FullName -Filter *.apppackage | ForEach-Object {Get-Content -Path $_.FullName})
-                If ($AppPackageXML) {
-                    #Found app descriptor, creating MSI Application
-                    New-MSIPackage -Source $SourceFolder -Publisher $Publisher -Name $Name -Version $Version -Description $Description -PackageDest $PackageDest\MSI -Descriptor $AppPackageXML
-                    If ($? -ne $True) {
-                        Write-Error "Failed to create application"
-                        return
-                    }
-                } Else {
                     return
                 }
             } Else {
                 Write-Error "XML file .apppackage not found."
                 return
             }
-            Default {
-                [xml]$AppPackageXML = (Get-ChildItem -Path $SourceFolder.FullName -Filter *.apppackage | ForEach-Object {Get-Content -Path $_.FullName})
-                If ($AppPackageXML) {
-                    #Found app descriptor, creating Custom Application
-                    New-CustomPackage -Source $SourceFolder -Publisher $Publisher -Name $Name -Version $Version -Description $Description -PackageType $_ `
-                        -PackageDest $PackageDest -Descriptor $AppPackageXML
-                    If ($? -ne $True) {
-                        Write-Error "Failed to create application"
-                        return
-                    }
-                } Else {
+        }
+        Default {
+            $AppPackageXML = Get-AppXML $SourceFolder
+            If ($AppPackageXML) {
+                #Found app descriptor, creating Custom Application
+                New-CustomPackage -Source $SourceFolder `
+                    -Publisher $Publisher -Name $Name `
+                    -Version $Version -Description $Description `
+                    -PackageType $_ -PackageDest $PackageDest `
+                    -Descriptor $AppPackageXML
+                If ($? -ne $True) {
+                    Write-Error "Failed to create application"
                     return
                 }
             } Else {
@@ -143,7 +151,9 @@ Get-ChildItem -Path $PackageRoot | Where-Object {
 
     #region ContentDistribution
     Try {
-        Start-CMContentDistribution -Application (Get-CMApplication -Name $PackageName) -DistributionPointGroupName "Full Site" -EA SilentlyContinue
+        $CMApp = Get-CMApplication -Name $PackageName
+        Start-CMContentDistribution -Application $CMApp `
+            -DistributionPointGroupName "Full Site" -EA SilentlyContinue
     } Catch {
         Write-Warning "Content distribution failed. Might already be distributed"
     }
@@ -172,28 +182,32 @@ Get-ChildItem -Path $PackageRoot | Where-Object {
     Write-Output "Checking collections"
     Switch ($Target) {
         AllMachines {
+            $ColName = "$Publisher $Name Pilot Machines"
             $DeploymentSettings = (New-DeployHT),(New-DeployHT)
-            $DeploymentSettings[0].CollectionName = "$Publisher $Name Pilot Machines"
+            $DeploymentSettings[0].CollectionName = $ColName
             $DeploymentSettings[0].AvailableDateTime = (Get-Date)
             $DeploymentSettings[0].DeadlineDateTime = (Get-Date).AddDays(7)
             $DeploymentType = 'WKS'
         }
         StaffApproval {
+            $ColName = "$Publisher $Name Pilot Users"
             $DeploymentSettings = (New-DeployHT),(New-DeployHT)
-            $DeploymentSettings[0].CollectionName = "$Publisher $Name Pilot Users"
+            $DeploymentSettings[0].CollectionName = $ColName
             $DeploymentSettings[0].AvailableDateTime = (Get-Date)
             $DeploymentSettings[0].DeadlineDateTime = (Get-Date).AddDays(7)
             $DeploymentSettings[1].CollectionName = "All USC Staff Users"
             $DeploymentSettings[1].ApprovalRequired = $True
             $DeploymentType = 'USR'
         }
-    } 
+    }
 
     #endreion
 
     #region CreateCollection
 
-    New-Collection -Type $DeploymentType -ColName $DeploymentSettings[0].CollectionName # Createh pilot collection
+    # Createh pilot collection
+    New-Collection -Type $DeploymentType `
+        -ColName $DeploymentSettings[0].CollectionName
     Write-Verbose "Sleeping..."
     Start-Sleep -Seconds 10
 
@@ -205,11 +219,12 @@ Get-ChildItem -Path $PackageRoot | Where-Object {
     # Create Each Deployment
     Write-Verbose "Checking deployment"
     $DeploymentSettings | ForEach-Object {
-        $CollectionName = $psItem.CollectionName
-        $Deployment = Get-CMDeployment -CollectionName $CollectionName -SoftwareName $PackageName
+        $CollName = $psItem.CollectionName
+        $Deployment = Get-CMDeployment -CollectionName $CollName `
+            -SoftwareName $PackageName
         If (!$Deployment) {
-            New-CMApplicationDeployment @psItem 
             Write-Verbose "Creating deployment of $($psItem.Name) to $CollName"
+            New-CMApplicationDeployment @psItem
             Write-Verbose "Created Deployment for $($psItem.Name)"
         }
 
@@ -220,7 +235,8 @@ Get-ChildItem -Path $PackageRoot | Where-Object {
     Write-Verbose "Moving source files to complete folder."
     If (Test-Path -Path "$CompletedRoot\$($SourceFolder.Name)") {
         #Package previously completed. Lets rename the old one
-        Rename-Item -Path "$CompletedRoot\$($SourceFolder.Name)" -NewName "$($SourceFolder.Name)_Renamed_Duplicate"
+        Rename-Item -Path "$CompletedRoot\$($SourceFolder.Name)" `
+            -NewName "$($SourceFolder.Name)_Renamed_Duplicate"
     }
     Move-Item $SourceFolder.FullName $CompletedRoot -Force
     Send-EmailMessage -Message "$Publisher $Name $Version has been auto imported to SCCM! Check the logs at General\Logs\PackageOrchestrator.log" -EmailAddress '3b7f44bd.usceduau.onmicrosoft.com@apac.teams.ms' -Subject "$Name $Version"
