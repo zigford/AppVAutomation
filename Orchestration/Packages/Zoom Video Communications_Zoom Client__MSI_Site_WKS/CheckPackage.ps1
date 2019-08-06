1d4e284527e72eb0bbfe520e191eb4d4d88f0bf3
Jesse Harris <zigford@gmail.com>
2019-08-06 18:05:00 +1000

    wip zoom package

---
+++ Orchestration/Packages/Zoom Video Communications_Zoom Client__MSI_Site_WKS/CheckPackage.ps1
@@ -0,0 +1,40 @@
+﻿[CmdLetBinding(SupportsShouldProcess)]
+Param()
+#Zoom Video Communications_Zoom Client__MSI_Site_WKS
+#Check for new versions of Zoom Client.
+#
+#1. Get old versions
+#2. Compare with current version
+#3. Queue package as nessecary
+# Settings
+# PackageDest = Where packages end up in production. We scan this location
+#     to see if we have the latest
+# PackageSource = A UNC Path where the package will be uploaded to for
+#     processing after either sequencing or downloading
+# PackageQueue = A filepath (local or remote), where a sequencer script
+#     should be deposited for processing. This tells the sequencer how
+#     to make the package and is the primary responsibility of this script
+# PackageName = Vendor_Application_Latest_APPV_WKS. Used to help determine 
+#     resulting package name, and find if a previous version already
+#     exists, or if the current version is still the latest
+#
+#     
+Import-Module "$PSScriptRoot\..\..\Functions\InstallFunctions.psm1" `
+    -Verbose:$False
+Import-Settings | Set-Variable Settings
+$PackageProperties = @{
+    Settings = $Settings
+    InstallScript = 
+@'
+start /wait msiexec.exe /I "<DLFILE>" /qb REBOOT=REALLYSUPPRESS'
+del "C:\Users\Public\Desktop\VLC media player.lnk"
+'@
+    <# FixList = "DisableObjects" -- Example #>
+    <# PreReq = 'notepad.exe' -- Example #>
+    URL = 'http://download.videolan.org/pub/videolan/vlc'
+    URLFunction = 'Get-VLCDownloadLink -Type MSI'
+}
+
+$PackageProperties | Select-NewerPackageVersion | New-AppPackageManifest
+
+Remove-Module InstallFunctions
